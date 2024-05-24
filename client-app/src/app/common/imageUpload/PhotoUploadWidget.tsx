cd0d744e649d70d7b9b1cd79afe8c8b6dd23acac
HubOneX <hubciox@gmail.com>
2024-05-25 01:05:31 +0200

    Added the react cropper

--- client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client-app/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,9 +1,25 @@
-import { Grid, Header, Image } from "semantic-ui-react";
+import { Button, Grid, Header } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 const PhotoUploadWidget = () => {
-  const [files, setFiles] = useState<any>([]);
+  const [files, setFiles] = useState<(object & { preview?: string })[]>([]);
+  const [cropper, setCropper] = useState<Cropper>();
+
+  const onCrop = () => {
+    if (cropper) {
+      cropper.getCroppedCanvas().toBlob((blob) => console.log(blob));
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      files.forEach((file: object & { preview?: string }) =>
+        URL.revokeObjectURL(file.preview!)
+      );
+    };
+  });
 
   return (
     <Grid>
@@ -15,12 +31,27 @@ const PhotoUploadWidget = () => {
       <Grid.Column width={4}>
         <Header sub color="teal" content="Step 2 - Resize Image" />
         {files && files.length > 0 && (
-            <Image src={files[0].preview} />
+          <PhotoWidgetCropper
+            setCropper={setCropper}
+            imagePreview={files[0].preview!}
+          />
         )}
       </Grid.Column>
       <Grid.Column width={1} />
       <Grid.Column width={4}>
         <Header sub color="teal" content="Step 3 - Preview & Upload" />
+        {files && files.length > 0 && (
+          <>
+            <div
+              className="img-preview"
+              style={{ minHeight: 200, overflow: "hidden" }}
+            />
+            <Button.Group widths={2}>
+              <Button onClick={onCrop} positive icon="check" />
+              <Button onClick={() => setFiles([])} icon="close" />
+            </Button.Group>
+          </>
+        )}
       </Grid.Column>
     </Grid>
   );
